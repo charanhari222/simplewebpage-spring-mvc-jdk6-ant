417e57bde51853bd912fb508f04173538c184bef
charanhari222 <79359215+charanhari222@users.noreply.github.com>
2021-11-17 13:04:28 +0530

    Update application-start-hook.sh

--- application-start-hook.sh
+++ application-start-hook.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-cd /temp/
-cp *.war /home/ec2-user/apache-tomcat-8.5.72/webapps/
+cd apache-tomcat-8.5.72/webapps/
+rm *.war
+aws s3 cp s3://antbucketforartifacts/home.war /home/ec2-user/apache-tomcat-8.5.72/webapps/
